3d62bcf54f447f1642523b2776f4a6e2f816932a
Samu <Pedroqueiroz.zen@gmail.com>
2024-11-26 21:35:00 -0300

    [shared/attribute] add

---
+++ Shared/Attribute.lua
@@ -0,0 +1,25 @@
+local FusionHelper = require(script.Parent.FusionHelper)
+
+local Attribute = {}
+
+function Attribute.SyncFromAttribute(instance: Instance, attribute: string, callback: (any) -> nil, ignoreFirst: boolean): RBXScriptConnection
+    if not ignoreFirst then
+        callback(instance:GetAttribute(attribute))
+    end
+    return instance:GetAttributeChangedSignal(attribute):Connect(function()
+        callback(instance:GetAttribute(attribute))
+    end)
+end
+
+function Attribute.SyncFusionToAttribute(instance: Instance, attribute: string, scope, value)
+    FusionHelper.ObserverCallback(scope, value, function(result)
+        instance:SetAttribute(attribute, result)
+    end)
+end
+
+function Attribute.Increment(instance: Instance, attribute: string, increment: number)
+    local now = instance:GetAttribute(attribute)
+    instance:SetAttribute(attribute, now + increment)
+end
+
+return Attribute
