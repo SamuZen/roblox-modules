2f99ded1c12de01b24b0d0145088303a5b8f47de
Samu <Pedroqueiroz.zen@gmail.com>
2024-11-26 21:34:51 -0300

    [shared/fusionHelper] add

---
+++ Shared/FusionHelper.lua
@@ -0,0 +1,17 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Fusion = require(ReplicatedStorage.Source.Fusion)
+local Observer = Fusion.Observer
+
+
+local FusionHelper = {}
+
+function FusionHelper.ObserverCallback(scope, watching, callback)
+    local observer = Observer(scope, watching)
+    callback(Fusion.peek(watching))
+    return observer:onChange(function()
+        callback(Fusion.peek(watching))
+    end)
+end
+
+return FusionHelper
