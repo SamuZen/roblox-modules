01200d9a0a2993236bb6ac3c7926956ad9a7688d
Samu <Pedroqueiroz.zen@gmail.com>
2024-12-03 00:23:20 -0300

    [utility/playSoundFromSource] add

---
+++ Shared/Utility/playSoundFromSource.lua
@@ -0,0 +1,19 @@
+--[[
+	playSoundFromSource - A utility function to clone and play a sound from a specified source,
+	destroying it once it has finished playing.
+--]]
+
+local function playSoundFromSource(soundTemplate: Sound, source: Instance, pitchAdjustment: number?)
+	local sound = soundTemplate:Clone()
+	if pitchAdjustment then
+		sound.PlaybackSpeed *= pitchAdjustment
+	end
+	sound.Parent = source
+
+	sound:Play()
+	sound.Ended:Once(function()
+		sound:Destroy()
+	end)
+end
+
+return playSoundFromSource
