99c3cf1b128823d40b4244c32cb47ed5e199afb9
Pedro Queiroz <Pedroqueiroz.zen@gmail.com>
2026-04-17 14:02:49 -0300

    add compact number string

--- Shared/Math.lua
+++ Shared/Math.lua
@@ -1,5 +1,7 @@
 local Math = {}
 
+local SUFFIXES = { "", "K", "M", "B", "T", "Qa", "Qi" }
+
 Math.Lerp = function(min, max, p)
     return min + ((max-min) * p)
 end
@@ -12,6 +14,16 @@ end
 Math.FloorDecimal = function(v, num)
     local r = 10 ^ num
     return math.floor(v * r) / r
+end
+
+Math.FormatCompact = function(n)
+	if n < 1000 then
+		return tostring(n)
+	end
+	local i = math.floor(math.log(n, 1000))
+	local short = math.floor(n / (1000 ^ i) * 10) / 10
+	local fmt = if short % 1 == 0 then "%d%s" else "%.1f%s"
+	return string.format(fmt, short, SUFFIXES[i + 1])
 end
 
 return Math
